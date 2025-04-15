03065d10ee88359b7b8bf649a98f75571d69cb0d
pardoma <pardoma@uji.es>
2025-04-15 16:39:53 +0200

    feat: Create Material Form Structure

--- src/app/components/materialForm/materialForm.component.ts
+++ src/app/components/materialForm/materialForm.component.ts
@@ -1,16 +1,24 @@
-import { Component} from "@angular/core";
+import { Component, Input} from "@angular/core";
 import { TrainingMaterial } from "../../model/trainingMaterial";
+import { InputTextModule } from "primeng/inputtext";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { FormsModule } from "@angular/forms";
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { TextareaModule } from 'primeng/textarea';
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
   selector: 'material-form',
   templateUrl: './materialForm.component.html',
   styleUrls: ['./materialForm.component.css'],
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule, FormsModule, InputIconModule, IconFieldModule, TextareaModule, StepperModule, ButtonModule],
 })
 export class MaterialFormComponent {
 
-  material: TrainingMaterial | undefined;
+  @Input() material: TrainingMaterial = new TrainingMaterial();
 
   selectedConcepts: string[] = [];
   
--- src/app/model/trainingMaterial.ts
+++ src/app/model/trainingMaterial.ts
@@ -1,25 +1,51 @@
-export interface TrainingMaterial {
-    title: string;
-    creator: string[];
-    publisher: string;
-    subject: string[];
-    abstract: string;
-    tableOfContents: string;
-    description: string;
-    contributor: string[];
-    created: string;
-    type: string;
-    format: string;
-    language: string;
-    SizeOrDuration: string;
-    audience: string;
-    educationLevel: string;
-    source: string;
-    rightsHolder: string;
-    license: string;
-    relation: string[];
-    image: string;
-    userId: string;
-    organization: string;
-    division: string;
+export class TrainingMaterial {
+  title: string;
+  creator: string[];
+  publisher: string;
+  subject: string[];
+  abstract: string;
+  tableOfContents: string;
+  description: string;
+  contributor: string[];
+  created: string;
+  type: string;
+  format: string;
+  language: string;
+  SizeOrDuration: string;
+  audience: string;
+  educationLevel: string;
+  source: string;
+  rightsHolder: string;
+  license: string;
+  relation: string[];
+  image: string;
+  userId: string;
+  organization: string;
+  division: string;
+
+  constructor(data?: Partial<TrainingMaterial>) {
+    this.title = data?.title ?? '';
+    this.creator = data?.creator ?? [];
+    this.publisher = data?.publisher ?? '';
+    this.subject = data?.subject ?? [];
+    this.abstract = data?.abstract ?? '';
+    this.tableOfContents = data?.tableOfContents ?? '';
+    this.description = data?.description ?? '';
+    this.contributor = data?.contributor ?? [];
+    this.created = data?.created ?? '';
+    this.type = data?.type ?? '';
+    this.format = data?.format ?? '';
+    this.language = data?.language ?? '';
+    this.SizeOrDuration = data?.SizeOrDuration ?? '';
+    this.audience = data?.audience ?? '';
+    this.educationLevel = data?.educationLevel ?? '';
+    this.source = data?.source ?? '';
+    this.rightsHolder = data?.rightsHolder ?? '';
+    this.license = data?.license ?? '';
+    this.relation = data?.relation ?? [];
+    this.image = data?.image ?? '';
+    this.userId = data?.userId ?? '';
+    this.organization = data?.organization ?? '';
+    this.division = data?.division ?? '';
   }
+}
