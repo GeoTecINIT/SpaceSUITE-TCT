200cbe7cd173ae450489dfee5c87fc79402f10fe
pardoma <pardoma@uji.es>
2025-11-05 11:26:42 +0100

    feat: Update Model

---
+++ src/app/model/trainingItem.ts
@@ -0,0 +1,108 @@
+export class TrainingItem {
+  // SpaceSuite Metadata
+  _id: string;
+  title: string
+  creators: string[];
+  subject: string[];
+  description: string;
+  abstract: string;
+  learningOutcomes: string[];
+  audience: string[];
+  created: any;
+  materialType: string[];
+  publisher: string;
+  url: string;
+  language?: string;
+  source?: string;
+  educationLevel: string[];
+  tableOfContents: string[];
+  workload: number;
+  prerequisites: string[];
+  assessment: string[];
+  concepts: string[];
+  
+  // Training Catalogue Metadata
+  image: string;
+  orgId?: string;
+  orgName?: string;
+  division?: string;
+  userId: string;
+  isPublic: boolean;
+  updatedAt: any;
+
+  // BMT required Metadata
+  collection: string;
+  collectionDisplay: string;
+  type: number;
+  hasMetadata: string;
+
+  constructor(data?: Partial<TrainingItem>) {
+    this._id = data?._id ?? '';
+    this.title = data?.title ?? '';
+    this.creators = data?.creators ?? [];
+    this.subject = data?.subject ?? [];
+    this.description = data?.description ?? '';
+    this.abstract = data?.abstract ?? '';
+    this.learningOutcomes = data?.learningOutcomes ?? [];
+    this.audience = data?.audience ?? [];
+    this.created = data?.created ?? new Date();
+    this.materialType = data?.materialType ?? [];
+    this.publisher = data?.publisher ?? '';
+    this.url = data?.url ?? '';
+    this.language = data?.language;
+    this.source = data?.source;
+    this.educationLevel = data?.educationLevel ?? [];
+    this.tableOfContents = data?.tableOfContents ?? [];
+    this.workload = data?.workload ?? 0;
+    this.prerequisites = data?.prerequisites ?? [];
+    this.assessment = data?.assessment ?? [];
+    this.concepts = data?.concepts ?? [];
+    this.image = data?.image ?? '';
+    this.orgId = data?.orgId;
+    this.orgName = data?.orgName;
+    this.division = data?.division;
+    this.userId = data?.userId ?? '';
+    this.isPublic = data?.isPublic ?? true;
+    this.updatedAt = data?.updatedAt ?? new Date();
+    this.collection = data?.collection ?? 'TrainingMaterials';
+    this.collectionDisplay = data?.collectionDisplay ?? 'TrainingMaterials';
+    this.type = data?.type ?? 4;
+    this.hasMetadata = data?.hasMetadata ?? 'True';
+  }
+
+  public toPlain(): Record<string, any> {
+    return {
+      _id: this._id,
+      title: this.title,
+      creators: this.creators,
+      subject: this.subject,
+      description: this.description,
+      abstract: this.abstract,
+      learningOutcomes: this.learningOutcomes,
+      audience: this.audience,
+      created: this.created,
+      materialType: this.materialType,
+      publisher: this.publisher,
+      url: this.url,
+      language: this.language,
+      source: this.source,
+      educationLevel: this.educationLevel,
+      tableOfContents: this.tableOfContents,
+      workload: this.workload,
+      prerequisites: this.prerequisites,
+      assessment: this.assessment,
+      concepts: this.concepts,
+      image: this.image,
+      orgId: this.orgId,
+      orgName: this.orgName,
+      division: this.division,
+      userId: this.userId,
+      isPublic: this.isPublic,
+      updatedAt: this.updatedAt,
+      collection: this.collection,
+      collectionDisplay: this.collectionDisplay,
+      type: this.type,
+      hasMetadata: this.hasMetadata
+    };
+  }
+}
--- src/app/model/trainingMaterial.ts
+++ src/app/model/trainingMaterial.ts
@@ -1,65 +1,23 @@
-import { OldTrainingMaterial } from "./oldTrainingMaterial";
+import { TrainingItem } from "./trainingItem";
 
-export class TrainingMaterial extends OldTrainingMaterial {
-  publisher: string;
-  created: any;
-  language?: string;
-  materialType: string[];
+export class TrainingMaterial extends TrainingItem {
   interactivityType?: string;
-  educationLevel: string[];
-  audience: string[];
-  assessment: string[];
-  prerequisites: string[];
-  learningOutcomes: string[];
-  tableOfContents: string[];
+  contributors: string[];
   license?: string;
-  source: string | undefined;
-  subject: string[];
-  image: string;
-  division?: string;
-  workload: number;
 
   constructor(data?: Partial<TrainingMaterial>) {
     super(data);
-    this.publisher = data?.publisher ?? '';
-    this.created = data?.created ?? new Date();
-    this.language = data?.language ?? undefined;
-    this.materialType = data?.materialType ?? [];
-    this.interactivityType = data?.interactivityType ?? undefined;
-    this.educationLevel = data?.educationLevel ?? [];
-    this.audience = data?.audience ?? [];
-    this.assessment = data?.assessment ?? [];
-    this.prerequisites = data?.prerequisites ?? [];
-    this.learningOutcomes = data?.learningOutcomes ?? [];
-    this.tableOfContents = data?.tableOfContents ?? [];
-    this.license = data?.license ?? undefined;
-    this.source = data?.source ?? undefined;
-    this.subject = data?.subject ?? [];
-    this.image = data?.image ?? '';
-    this.division = data?.division ?? undefined;
-    this.workload = data?.workload ?? 0;
+    this.interactivityType = data?.interactivityType;
+    this.contributors = data?.contributors ?? [];
+    this.license = data?.license;
   }
 
   override toPlain(): Record<string, any> {
     return {
       ...super.toPlain(),
-      publisher: this.publisher,
-      created: this.created,
-      language: this.language,
-      materialType: this.materialType,
       interactivityType: this.interactivityType || "",
-      educationLevel: this.educationLevel,
-      audience: this.audience,
-      assessment: this.assessment,
-      prerequisites: this.prerequisites,
-      learningOutcomes: this.learningOutcomes,
-      tableOfContents: this.tableOfContents,
-      license: this.license,
-      source: this.source || "",
-      subject: this.subject,
-      image: this.image,
-      division: this.division,
-      workload: this.workload
+      contributors: this.contributors,
+      license: this.license
     };
   }
 }
